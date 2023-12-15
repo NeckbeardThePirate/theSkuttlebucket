e7c47f2649d18c68122993af79f17a09acfc4f9a
judah helland <judah@hellands.com>
2023-12-15 15:52:49 -0800

    file structure changes and security updates

--- skuttlebucket/theBarnYard.js
+++ skuttlebucket/theBarnYard.js
@@ -3,16 +3,8 @@ import { getFirestore, collection, query, where, addDoc, updateDoc, getDocs, doc
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/10.4.0/firebase-auth.js";
 // import { docSnap } from "./userProfile";
 
+import { firebaseConfig } from "../firebaseConfig.js";
 
-const firebaseConfig = {
-    apiKey: "AIzaSyAnvmVyCWmYiQlbXa8kF_bYeKbLmf8_Rhk",
-    authDomain: "theskuttlebucket.firebaseapp.com", 
-    projectId: "theskuttlebucket",
-    storageBucket: "theskuttlebucket.appspot.com",
-    messagingSenderId: "694795060337",
-    appId: "1:694795060337:web:587136163506c2f83d47d0",
-    measurementId: "G-BLWVYSCCS5"
-    };
 
 const app = initializeApp(firebaseConfig);
 
