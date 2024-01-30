b7dd92d7eb66a39681e2117d19af6d5afad4bd7d
judah helland <judah@hellands.com>
2024-01-29 16:09:07 -0800

    dunno what happened here...

---
+++ firebaseConfig.js
@@ -0,0 +1,9 @@
+export var firebaseConfig = {
+    apiKey: "AIzaSyAnvmVyCWmYiQlbXa8kF_bYeKbLmf8_Rhk",
+    authDomain: "theskuttlebucket.firebaseapp.com",
+    projectId: "theskuttlebucket",
+    storageBucket: "theskuttlebucket.appspot.com",
+    messagingSenderId: "694795060337",
+    appId: "1:694795060337:web:587136163506c2f83d47d0",
+    measurementId: "G-BLWVYSCCS5"
+}
